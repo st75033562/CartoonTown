eb37bc99fb237a36e48581e84c93f2168b048c37
xiaxiaohu <xxh@g3games.cn>
2019-12-10 16:11:14 +0800

    init

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform follow;        //the position of Player
+
+    private float distanceAway = 15.0f;
+    private float distanceUp = 5.0f;
+    private float smooth = 2f;               // how smooth the camera movement is
+
+    private Vector3 m_TargetPosition;       // the position the camera is trying to be in)
+
+    void Start()
+    {
+        transform.LookAt(follow);
+    }
+
+    void LateUpdate()
+    {
+        m_TargetPosition = follow.position + Vector3.up * distanceUp + Vector3.forward * distanceAway;
+
+        transform.position = Vector3.Lerp(transform.position, m_TargetPosition, Time.deltaTime * smooth);
+        
+    }
+}
---
+++ Assets/MoveControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl : MonoBehaviour {
+
+    public Transform rotaeTransform;
+    public Animator animator;
+    private CharacterController cc;
+
+    private float speed = 3;
+    private float rotateSpeed = 50;
+
+    void Start()
+    {
+        cc = this.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (ETCInput.GetAxis("Vertical") != 0)
+        {
+            animator.ResetTrigger("idle");
+            animator.SetTrigger("walk");
+        }
+
+        if (ETCInput.GetAxis("Vertical") == 0 && ETCInput.GetAxis("Horizontal") == 0)
+        {
+            animator.ResetTrigger("walk");
+            animator.SetTrigger("idle");
+        }
+
+        //if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    character.Move(-transform.forward * speed * Time.deltaTime);
+        //    animator.ResetTrigger("idle");
+        //    animator.SetTrigger("walk");
+        //}
+        //else
+        //{
+        //    animator.ResetTrigger("walk");
+        //    animator.SetTrigger("idle");
+        //}
+
+        //if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+        //    animator.ResetTrigger("walk");
+        //    animator.SetTrigger("idle");
+        //}
+        //else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        //    animator.ResetTrigger("walk");
+        //    animator.SetTrigger("idle");
+        //}
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.name == "box")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
